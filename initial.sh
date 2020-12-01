4864110530439652cebc06f05324758263908c2f
pureDead <rapidfire0306@gmail.com>
2020-12-02 00:52:59 +0100

    tidyup

--- initial.sh
+++ initial.sh
@@ -1,22 +1,4 @@
 #pacman
-pacman -Syu;
-pacman -S --needed --noconfirm git john nmap hashcat netcat metasploit skypeforlinux-stable-bin hydra nikto
-
-#aur
-mkdir ~/aur
-cd ~/aur
-git clone https://aur.archlinux.org/burpsuite.git
-git clone https://aur.archlinux.org/seclists.git
-git clone https://aur.archlinux.org/polenum.git
-git clone https://aur.archlinux.org/openldap-ppm.git
-git clone https://aur.archlinux.org/enum4linux.git
-git clone https://aur.archlinux.org/exploit-db-git.git
-git clone https://aur.archlinux.org/gobuster.git
-git clone https://aur.archlinux.org/peass.git
-git clone https://aur.archlinux.org/hash-identifier.git
-for i in `ls`; do
-	cd $i
-  git pull origin
-	makepkg -si --noconfirm
-	cd ..
-done
+sudo pacman -Syu;
+sudo pacman -S --needed --noconfirm git john nmap hashcat netcat metasploit skypeforlinux-stable-bin hydra nikto
+yay -S --needed  burpsuite seclists exploit-db-git gobuster-git hash-identifier
