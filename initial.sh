0df005ee36f117cf1703e94e84dbd80732cf84c2
pureDead <rapidfire0306@gmail.com>
2020-12-01 12:48:48 +0100

    baseIter

--- initial.sh
+++ initial.sh
@@ -1,9 +1,22 @@
+#pacman
 pacman -Syu;
-pacman -s --needed git;
-cd ~/;
-mkdir aur;
-cd aur;
-git clone https://aur.archlinux.org/burpsuite.git;
-cd burpsuite;
-git pull origin;
-makepkg -si;
+pacman -S --needed --noconfirm git john nmap hashcat netcat metasploit skypeforlinux-stable-bin hydra nikto
+
+#aur
+mkdir ~/aur
+cd ~/aur
+git clone https://aur.archlinux.org/burpsuite.git
+git clone https://aur.archlinux.org/seclists.git
+git clone https://aur.archlinux.org/polenum.git
+git clone https://aur.archlinux.org/openldap-ppm.git
+git clone https://aur.archlinux.org/enum4linux.git
+git clone https://aur.archlinux.org/exploit-db-git.git
+git clone https://aur.archlinux.org/gobuster.git
+git clone https://aur.archlinux.org/peass.git
+git clone https://aur.archlinux.org/hash-identifier.git
+for i in `ls`; do
+	cd $i
+  git pull origin
+	makepkg -si --noconfirm
+	cd ..
+done
